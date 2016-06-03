0eaefb29298e99cfd77d563a95622fe9e477ba74
csjcode <christophersstjohn@gmail.com>
2016-06-03 17:29:46 +0700

    Template for rendering react App component

--- client/main.js
+++ client/main.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+const App = () => {
+   return (
+      <div>Hello there!</div>
+   );
+};
+
 // After Meteor loads, render ap to dropdown-menu
 
 Meteor.startup(() => {
    // React render call
+   ReactDOM.render(<App />, document.querySelector('.container'))
 });
