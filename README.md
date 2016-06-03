092451811953956ebdbdcf430a4f7df397b98116
csjcode <christophersstjohn@gmail.com>
2016-06-03 17:21:21 +0700

    Initial Meteor config and mockups

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-meteor-employees
 Employee Directory with React &amp; Meteor
+
+Demo of creating an employee directory with React and Meteor, based on the Udemy course
---
+++ client/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// After Meteor loads, render ap to dropdown-menu
+
+Meteor.startup(() => {
+   // React render call
+});
