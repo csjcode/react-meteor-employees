3a1bf47f940133d335bb9f32ac61f9b6892e424a
csjcode <christophersstjohn@gmail.com>
2016-06-04 13:16:46 +0700

    Setup React-Meteor Publish-Subscribe Container

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,7 +1,9 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
+import Employees from '../../imports/collections/employees';
 
 const EmployeeList = () => {
+      // if props include in above function argurment, here you can access props.employees => array of employee objects 
       return (
          <div>
             <div className="employee-list">
@@ -11,4 +13,11 @@ const EmployeeList = () => {
       );
 };
 
-export default EmployeeList;
+export default creatContainer(() => {
+      // setup subscription
+      Meteor.subscribe('employees');
+
+      // return an object. Whatever we return sent to EmployeeList as props
+      return { employees: Employees.find({}).fetch()}
+
+},EmployeeList);
