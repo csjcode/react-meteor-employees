46d5a097a254669a1769d3933537b5702e6adf97
csjcode <christophersstjohn@gmail.com>
2016-06-04 12:49:37 +0700

    EmployeeList component setup

---
+++ client/components/employee_list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+const EmployeeList = () => {
+      return (
+         <div>
+            <div className="employee-list">
+               Employee List
+            </div>
+         </div>
+      );
+};
+
+export default EmployeeList;
