fa33df0ed87b6365a15f01c17ab511d2ceadd6bf
csjcode <christophersstjohn@gmail.com>
2016-06-04 13:38:31 +0700

    Fixed handlebar typo in import

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
-import Employees from '../../imports/collections/employees';
+import { Employees } from '../../imports/collections/employees';
 
-const EmployeeList = () => {
-      // if props include in above function argurment, here you can access props.employees => array of employee objects 
-      return (
-         <div>
-            <div className="employee-list">
-               Employee List
-            </div>
-         </div>
-      );
+const EmployeeList = (props) => {
+  // props.employees => an array of employee objects
+
+  return (
+    <div>
+      <div className="employee-list">
+        Employee List
+      </div>
+    </div>
+  );
 };
 
-export default creatContainer(() => {
-      // setup subscription
-      Meteor.subscribe('employees');
+export default createContainer(() => {
+  // set up subscription
+  Meteor.subscribe('employees');
 
-      // return an object. Whatever we return sent to EmployeeList as props
-      return { employees: Employees.find({}).fetch()}
-
-},EmployeeList);
+  // return an object.  Whatever we return will be sent to EmployeeList
+  // as props
+  return { employees: Employees.find({}).fetch() };
+}, EmployeeList);
--- client/main.js
+++ client/main.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import EmployeeList  from './components/employee_list';
+
+import EmployeeList from './components/employee_list';
 
 const App = () => {
-   return (
-      <div><EmployeeList /></div>
-   );
+  return (
+    <div>
+      <EmployeeList />
+    </div>
+  );
 };
 
-// After Meteor loads, render ap to dropdown-menu
-
+// After Meteor loads in the browser, render my app to the DOM.
 Meteor.startup(() => {
-   // React render call
-   ReactDOM.render(<App />, document.querySelector('.container'))
+  // React render call
+  ReactDOM.render(<App />, document.querySelector('.container'));
 });
