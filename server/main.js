1291a11d42e335748ce81258f95d735656fe7bb5
csjcode <christophersstjohn@gmail.com>
2016-06-04 12:03:30 +0700

    Fixed import typo & others

--- server/main.js
+++ server/main.js
@@ -2,21 +2,22 @@
 import _ from 'lodash'
 import { Meteor } from 'meteor/meteor';
 import { Employees } from '../imports/collections/employees';
-import image,helpers from 'faker';
+import {image,helpers} from 'faker';
 
 Meteor.startup(() => {
    // Good place to generate data-toggle
 
    // Check to see if data exists in collection
    const numberRecords = Employees.find({}).count();
+   console.log(numberRecords);
    if (!numberRecords){
       // Generate some data
       _.times(5000, () => {
          const { name, email, phone } = helpers.createCard();
 
-         Employes.insert({
+         Employees.insert({
             name, email, phone,
-            avatar: image.avatar();
+            avatar: image.avatar()
          })
       });
    }
